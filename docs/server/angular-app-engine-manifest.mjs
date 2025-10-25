0b60684065b8824de34b87103982934b021471c4
parmanand kosle <koslesonu8@gmail.com>
2025-10-25 20:24:59 +0530

    4rth commit

--- docs/server/angular-app-engine-manifest.mjs
+++ docs/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/',
+  basePath: '/Endoscope',
   supportedLocales: {
   "en-US": ""
 },
--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,24 +2,24 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/',
+  baseHref: '/Endoscope/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/"
+    "route": "/Endoscope"
   },
   {
     "renderMode": 2,
-    "route": "/productlist"
+    "route": "/Endoscope/productlist"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 3324, hash: '6b0e8a5c792a2878e2b5e30d104dc913c33e889fec3642e915e6ccbd0e02912d', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 2685, hash: '88211eb6a0dd9a9d725d4726384cfa20fe48ce31a5749207826b3aacedc63827', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'productlist/index.html': {size: 11333, hash: '240038a2480fa8588a2f7c44c13bb6da0ad7dc47cd4f631b28c3a8b8b6682b02', text: () => import('./assets-chunks/productlist_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 56661, hash: 'd1514a8243f5a74fbb4948f66ae06bad8a47f4678ae108b27bfbab14ae5a0942', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 3334, hash: '816d2912ebf16ef8976874cc27f3f7435a406cb65521c404c4bbd2b52c3ca99e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 2695, hash: '933cb57d9f2e083e53871571fc8d00d19d28d851960c1c27403c32d7cfd3c090', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'productlist/index.html': {size: 11345, hash: 'b5c566c0d16ad14af29a6438f3437effd38afc32e69d31b4c000e5d622c7fbd5', text: () => import('./assets-chunks/productlist_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 56670, hash: 'd916795d23c589e3240672f6fb87abfc69e0947943ff37fe066a82913a7914b5', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-56A33T4J.css': {size: 199579, hash: 'S65wfUkwwEM', text: () => import('./assets-chunks/styles-56A33T4J_css.mjs').then(m => m.default)}
   },
 };
